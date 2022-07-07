6e088dbd11a075c0a9bead3f0b3a87ba71718ac0
corey <coreymmartin1991@gmail.com>
2022-07-07 13:31:20 -0500

    exercise 1 yeye

--- Testing/Startup.cs
+++ Testing/Startup.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MySql.Data.MySqlClient;
 
 namespace Testing
 {
@@ -23,6 +25,13 @@ namespace Testing
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IDbConnection>((s) =>
+            {
+                IDbConnection conn = new MySqlConnection(Configuration.GetConnectionString("bestbuy"));
+                conn.Open();
+                return conn;
+            });
+            services.AddTransient<IProductRepository, ProductRepository>();
             services.AddControllersWithViews();
         }
 
